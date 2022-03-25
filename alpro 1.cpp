0196e4788ec88292e2aa2a291bebebd4fe13d989
Kevinrznsptra <kevinrazansaputrasaputra@gmail.com>
2022-03-25 21:53:38 +0700

    file baru di studi kasus

--- alpro 1.cpp
+++ alpro 1.cpp
@@ -8,7 +8,18 @@ int main(){
 	cout<<" Gaji tim 2 = 28 juta"<<endl;
 	cout<<"\n Waktu pengerjaan TIM 1 (dalam bulan) : ";
 	cin>>waktu1;
-
+	
+	if (waktu1==5){
+		gaji1=22000000;
+		bonus1=22000000*0.06;
+		cout<<" Total Gaji Yang Anda Terima = "<<gaji1+bonus1<<endl;
+	}
+	else{
+		gaji1=22000000;
+		cout<<" Total Gaji Yang Anda Terima = "<<gaji1<<endl;
+	}
+	cout<<"\n Waktu pengerjaan Tim 2 (dalam bulan) : ";
+	cin>>waktu2;
 
 	if(waktu2==5){
 		gaji2=28000000;
