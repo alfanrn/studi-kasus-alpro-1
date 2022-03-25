bc47c8589f4362abadcf907fa7ca487e311afa8c
Kevinrznsptra <kevinrazansaputrasaputra@gmail.com>
2022-03-25 23:52:34 +0700

    file baru di kasus studi1

--- alpro 1.cpp
+++ alpro 1.cpp
@@ -9,17 +9,6 @@ int main(){
 	cout<<"\n Waktu pengerjaan TIM 1 (dalam bulan) : ";
 	cin>>waktu1;
 	
-	if (waktu1==5){
-		gaji1=22000000;
-		bonus1=22000000*0.06;
-		cout<<" Total Gaji Yang Anda Terima = "<<gaji1+bonus1<<endl;
-	}
-	else{
-		gaji1=22000000;
-		cout<<" Total Gaji Yang Anda Terima = "<<gaji1<<endl;
-	}
-	cout<<"\n Waktu pengerjaan Tim 2 (dalam bulan) : ";
-	cin>>waktu2;
 
 	if(waktu2==5){
 		gaji2=28000000;
